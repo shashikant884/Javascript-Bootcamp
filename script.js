39447d75d73967564666756151d089d93e6af15b
shashikant884 <yadavshashikant884@gmail.com>
2024-03-08 17:10:16 +0530

    object part

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log('Hello....');
+// console.log('Hello....');
+// let school = null;
+// console.log(typeof school,"school");
+
+
+// const shashikant = {
+//     name: "Shashikant",
+//     age: 16,
+//     isAdult: false,
+//     rollnumber: 49,
+//     address:{
+//         pincode:40067,
+//         road:'link',
+//         district:'Mumbai'
+//     },
+//     subject:["maths","physics","english"],
+// };
+
+// const array =[1,2,23,5,6,7,8,];
+
+// console.log(typeof shashikant);
+// console.log(typeof array);
+
+
+// const array = [1,2,3,4,5];
+// console.log(typeof array, "Array");  // object
+
+
+const array = [1,2,3,4,5,'shashi',{name:'shashikant'}];
+
+console.log( array, "Array"); 
