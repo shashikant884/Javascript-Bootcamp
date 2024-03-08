86d40591df20fe23cc5614dea51edc8e31e1d24b
shashikant884 <yadavshashikant884@gmail.com>
2024-03-08 20:09:49 +0530

    basic of funtion

--- script.js
+++ script.js
@@ -26,6 +26,54 @@
 // console.log(typeof array, "Array");  // object
 
 
-const array = [1,2,3,4,5,'shashi',{name:'shashikant'}];
+// const array = [1,2,3,4,5,'shashi',{name:'shashikant'}];
 
-console.log( array, "Array"); 
+// console.log( array, "Array"); 
+
+// const str = "12";
+// const name = "12a"
+// console.log(+str);
+// console.log(Number(str));
+
+// console.log(+name);
+// console.log(Number(name));
+
+
+// const person = {
+//     name : "king",
+//     age : 12,
+//     "cur std" : 2,
+//     schoolName : "Dayanad memorial foundation",
+ 
+// };
+
+// console.log(person);
+// console.log(person["cur std"]);
+// console.log(person.schoolName);
+// delete person["cur std"];
+
+// console.log(person);
+
+// console.log(person["cur std"]);
+
+
+// const arrays = [1,2,3,4,5,6];
+// console.log(arrays.length);
+// console.log(arrays);
+
+// console.log(arrays[1]);
+
+// console.log(arrays.length-1);
+
+// arrays[2] =45;
+
+// console.log(arrays);
+
+
+const arr = [1,2,3,4,6,];
+
+console.log(arr.length);
+
+const newLengthOfArr = arr.push(100);
+
+console.log(arr, newLengthOfArr);
