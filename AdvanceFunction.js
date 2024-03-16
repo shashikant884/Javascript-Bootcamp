6128a1e3c72307bc3e35a40201e0260688cc83ad
shashikant884 <yadavshashikant884@gmail.com>
2024-03-16 12:42:27 +0530

    Advance Function0

---
+++ AdvanceFunction.js
@@ -0,0 +1,39 @@
+//console.log("Higher order function");
+
+//function statement 
+// function a(){
+//     console.log("From a function");
+// }
+// a();
+
+
+//Function expression
+// let b = function(){
+//     console.log("From function B");
+// }
+// b();
+
+//Function Declaration  aka function statement 
+// function b(){
+//     console.log("function declaration");
+// }
+// b();
+
+//Annnymous function
+
+// (function (){
+//     console.log("Annnymous function");
+// }())
+
+
+
+//Function expression
+// var b = function (param1 , param2){
+//     console.log(" call b");
+   
+// }
+// b(1, 2);
+//a();
+    
+
+ 
